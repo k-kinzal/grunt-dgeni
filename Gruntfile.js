b25f272e57b1e72ee114d7b52c6f67921a602f10
k-kinzal <kinzal@logn.in>
2014-09-20 14:43:14 +0900

    add Gruntfile,js

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,14 +11,59 @@
 module.exports = function (grunt) {
   // grunt configuration
   grunt.initConfig({
+	  clean: {
+	    server: '.tmp'
+	  },
+  	watch: {
+  		debug: {
+  			files: ['tasks/*.js'],
+  			tasks: ['jshint', 'dgeni']
+  		}
+  	},
+    shell: {
+      gitclone: {
+        command: 'git clone https://github.com/angular/angular.js.git .tmp/angular/'
+      }
+    },
+	  jshint: {
+	    options: {
+	      jshintrc: '.jshintrc',
+	      force: true,
+		    reporter: require('jshint-stylish')
+	    },
+	    all: [
+	      'tasks/*.js',
+	    ]
+	  },
+	  dgeni: {
+	  	options: {
+	  		packages: [
+	  			require('dgeni-markdown')
+	  		],	
+	  	},
+	  	src: ['.tmp/angular/src/**.js'],
+	  	build: '.tmp/build/',
+	  }
   });
-  // debugging
+  // tasks
+  grunt.registerTask('init', function() {
+  	grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.task.run([
+      'clean',
+      'shell:gitclone'
+    ]);
+  });
+  grunt.registerTask('debug', function() {
+    require('./tasks/dgeni')(grunt);
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-regarde');
+    grunt.renameTask('regarde', 'watch');
+    
+    grunt.task.run([
+      'watch'
+    ]);
+  });
   grunt.registerTask('test', function() {
-  });
-  grunt.registerTask('doc', function() {
-    grunt.warn('Grunt warning test.');
-  });
-  grunt.registerTask('fatal', function() {
-    grunt.fatal('Grunt fatal test.');
   });
 };
